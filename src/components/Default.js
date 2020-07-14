574719576a14cf12aa237885e1950658e5a57f38
Modeht <Modeht@users.noreply.github.com>
2020-07-14 21:25:22 +0200

    initial structure

---
+++ src/components/Default.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Default() {
+    return (
+        <div>
+            <h3 className="text-capitalize">page not found</h3>
+        </div>
+    )
+}
---
+++ src/components/ProductList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Product from './Product'
+export default function ProductList() {
+    return (
+        <div>
+            <Product></Product>
+            <h3>Hello from product list</h3>
+        </div>
+    )
+}
